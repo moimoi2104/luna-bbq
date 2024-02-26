8d262b3a7f8241e189fccea3e3a1d06361866cb0
Moises Romero <46231652+moimoi2104@users.noreply.github.com>
2024-02-26 12:15:09 -0500

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,2 @@
+// Contenido del archivo independiente mi-script.js
+document.write('<scr'+'ipt type="text/javascript" src="//pl22402447.profitablegatecpm.com/0b/81/cb/0b81cb8dff1c33fbc54ecfdd2f86e592.js"></scr'+'ipt>');
